93c342730f6f2b419a9eec0a89188ef9cf5c9475
yusupovlab <yusupovforwin@gmail.com>
2025-05-01 18:49:39 +0500

    added: MailConfiguration to send mail

---
+++ src/main/java/uz/muxtoriyat/config/MailConfiguration.java
@@ -0,0 +1,28 @@
+package uz.muxtoriyat.config;
+
+import java.util.Properties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+@Configuration
+public class MailConfiguration {
+
+    @Bean
+    public JavaMailSender getJavaMailSender() {
+        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+        mailSender.setHost("smtp.gmail.com");
+        mailSender.setPort(587);
+
+        mailSender.setUsername("yusupovforwin@gmail.com");
+        mailSender.setPassword("qazv rbrp fyuh tivb");
+
+        Properties props = mailSender.getJavaMailProperties();
+        props.put("mail.transport.protocol", "smtp");
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.debug", "true");
+        return mailSender;
+    }
+}

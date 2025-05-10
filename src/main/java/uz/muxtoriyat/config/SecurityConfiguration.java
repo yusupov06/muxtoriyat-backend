40d2767086893c52ca79e67846a7b49b95da49b7
yusupovlab <yusupovforwin@gmail.com>
2025-05-10 13:00:14 +0500

    configured SecurityConfiguration

--- src/main/java/uz/muxtoriyat/config/SecurityConfiguration.java
+++ src/main/java/uz/muxtoriyat/config/SecurityConfiguration.java
@@ -70,6 +70,8 @@ public class SecurityConfiguration {
                     .requestMatchers(mvc.pattern("/api/account/reset-password/init")).permitAll()
                     .requestMatchers(mvc.pattern("/api/account/reset-password/finish")).permitAll()
                     .requestMatchers(mvc.pattern("/api/admin/**")).hasAuthority(AuthoritiesConstants.ADMIN)
+                    .requestMatchers(mvc.pattern("/api/reactions/react")).permitAll()
+                    .requestMatchers(mvc.pattern("/api/view/articles")).permitAll()
                     .requestMatchers(mvc.pattern("/api/view/categories")).permitAll()
                     .requestMatchers(mvc.pattern("/api/view/sources")).permitAll()
                     .requestMatchers(mvc.pattern("/api/**")).authenticated()
